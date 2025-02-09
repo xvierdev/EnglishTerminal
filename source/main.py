42955bfb1e638be5eb2a1298e1f9c55f374fc8d1
Wesley Xavier <wesley.xvier@gmail.com>
2025-02-09 19:55:37 -0300

    docs: adicionado comentários para facilitar a compreensão.

--- source/main.py
+++ source/main.py
@@ -1,9 +1,12 @@
-points = 0
-while(True):
-    word = 'book'
-    answer = input(f'Whats is {word}?')
-    if answer == 'livro':
-        print('Thats hight!')
-        points += 1
-    else:
-        print('It\'s wrong.')
+points = 0      # a pontuação inicial do jogador é zero
+while(True):    # mantém o programa em loop indefinidamente
+    word = 'book' # primeira palavra que o jogador precisa acertar
+    answer = input(f'Whats is {word}?') # aqui a variável answer vai receber a resposta digitada do teclado
+    if answer == 'livro': # essa linha vai testar se a pessoa acertou a resposta
+        print('Thats hight!') # aqui é o congratulations do acerto
+        points += 1 # nesta linha aumenta a pontuação em +1
+    else: # aqui é caso a resposta esteja errada
+        print('It\'s wrong.') # imprime a mensagem de erro na tela
+        print(f'Points {points}') # mostra a pontuação final
+        input('Press enter to quit') # aguarda o usuário apertar enter
+        break # para o loop e o programa fecha
