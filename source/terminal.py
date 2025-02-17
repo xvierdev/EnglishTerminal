3023b2634e251a38063606fee81030741874681b
“Cubo3D” <davisilvaperes3@gmail.com>
2025-02-16 21:13:32 -0300

    novos comandos: neofetch e logs -r(ele apaga todos os textos do arquivo log)

--- source/terminal.py
+++ source/terminal.py
@@ -8,6 +8,7 @@ YELLOW = "\033[33m"
 BLUE = "\033[34m"
 MAGENTA = "\033[35m"
 CYAN = "\033[36m"
+BLACK = "\033[30m"
 RESET = "\033[0m"
 
 while True:
@@ -15,9 +16,27 @@ while True:
     if command == 'game':
         print("GAME!!")
         game()  # Agora o jogo roda e volta ao terminal
+
     elif command == 'logs':
         read()
+
     elif command == 'exit':
         break  # Sai do terminal corretamente
+
+    elif command == 'neofetch':
+        print(f"{RESET}|||||||||||||||||||||")
+        print(f"{RESET}|||{MAGENTA}/{BLACK}/////////////{RESET}||||")
+        print(f"{RESET}||{MAGENTA}//{BLACK}//{MAGENTA}//////////{RESET}|||||")
+        print(f"{RESET}||{MAGENTA}||{BLACK}//{MAGENTA}/{RESET}||||||||||||||")
+        print(f"{RESET}||{MAGENTA}||{BLACK}//{MAGENTA}/{RESET}||||||||||||||")
+        print(f"{RESET}||{MAGENTA}||{BLACK}/////////////{RESET}||||")
+        print(f"{RESET}||{MAGENTA}||{BLACK}//{MAGENTA}//////////{RESET}|||||")
+        print(f"{RESET}||{MAGENTA}{MAGENTA}||{BLACK}//{MAGENTA}/{RESET}||||||||||||||")
+        print(f"{RESET}||{MAGENTA}||{BLACK}//{MAGENTA}/{RESET}||||||||||||||")
+        print(f"{RESET}||{MAGENTA}||{BLACK}/////////////{RESET}||||")
+        print(f"{RESET}||{MAGENTA}//////////////{RESET}|||||")
+        print(f"{RESET}|||||||||||||||||||||")
+        #Imprime na tela...
+        
     else:
         print("command not found")
