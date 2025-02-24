aa83e42b06950015621c5ba08d89cabb76367849
Wesley Xavier <wesley.xvier@gmail.com>
2025-02-24 15:57:15 -0300

    chore: atualizado script util com tratamento de exceção e melhor legibilidade do código.

--- source/util.py
+++ source/util.py
@@ -1,46 +1,71 @@
-# Códigos de cores utilizados na formatação do terminal.
-RED = "\033[31m"
-GREEN = "\033[32m"
-YELLOW = "\033[33m"
-BLUE = "\033[34m"
-MAGENTA = "\033[35m"
-BLACK = "\033[30m"
-CYAN = "\033[36m"
-RESET = "\033[0m"
-
-# biblioteca de utilidades utilizdas na aplicação.
+# Script de utilidades, obtenção do tempo a atual, criptografia e calculo de hash.
 
 import hashlib
 import base64
 import datetime
 
-# retorna a data e o tempo atual.
-def now():
-  return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+#==============COLORS==============#
 
-# limpa a tela do console.
+BLACK = "\033[30m"
+BLUE = "\033[34m"
+CYAN = "\033[36m"
+GREEN = "\033[32m"
+MAGENTA = "\033[35m"
+RED = "\033[31m"
+YELLOW = "\033[33m"
+RESET = "\033[0m"
+
+#===============UTIL===============#
+
+# Retorna a data e hora formatadas.
+def now():
+  return datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+# Limpa a tela do console.
 def clear_console():
   print('\033[2J', end='')
   print('\033[H', end='')
 
-# calcula o hash do arquivo especificado.
+# Report de bugs
+
+def report_bug(error_msg):
+    try:
+        with open('error_log.txt', 'a', encoding="utf-8") as file:
+            file.write(f'{now()} {error_msg}\n')
+            print(f'Error: {error_msg}')
+    except Exception as e:
+        print(f"Erro ao reportar bug: {e}")
+
+
+#===========CRIPTOGRAFIA===========#
+
+# Calcula o hash do arquivo informado.
 def calcular_hash(arquivo):
-    with open(arquivo, 'rb') as f:
-        hash = hashlib.sha256()
-        while chunk := f.read(4096):
-            hash.update(chunk)
-    return hash.hexdigest()
+    try:
+      with open(arquivo, 'rb') as f:
+          hash = hashlib.sha256()
+          while chunk := f.read(4096):
+              hash.update(chunk)
+      return hash.hexdigest()
+    except Exception as e:
+       report_bug(e)
 
-# criptografa os dados de hash com a senha
+# Salva o hash criptografado no arquivo hash.txt
 def criptografar_hash(hash, senha):
-    senha_bytes = senha.encode()
-    hash_bytes = hash.encode()
-    criptografado = base64.b64encode(hash_bytes + senha_bytes)
-    return criptografado.decode()
+    try:
+      senha_bytes = senha.encode()
+      hash_bytes = hash.encode()
+      criptografado = base64.b64encode(hash_bytes + senha_bytes)
+      return criptografado.decode()
+    except Exception as e:
+      report_bug(e)
 
-# descriptografar dados de hash com a senha
+# Descriptografa o hash do arquivo de hash.
 def descriptografar_hash(criptografado, senha):
-    criptografado_bytes = criptografado.encode()
-    senha_bytes = senha.encode()
-    hash_bytes = base64.b64decode(criptografado_bytes)[:-len(senha_bytes)]
-    return hash_bytes.decode()
+    try:
+      criptografado_bytes = criptografado.encode()
+      senha_bytes = senha.encode()
+      hash_bytes = base64.b64decode(criptografado_bytes)[:-len(senha_bytes)]
+      return hash_bytes.decode()
+    except Exception as e:
+      report_bug(e)
