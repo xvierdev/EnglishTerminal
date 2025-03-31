b142a370de8f72bb20e8cf6e78342dde6a0583c6
Wesley Xavier <wesley.xvier@gmail.com>
2025-03-31 15:35:53 -0300

    modificação do arquivo main para simplificação do menu principal.

--- source/main.py
+++ source/main.py
@@ -1,9 +1,14 @@
-def menu():
+def menu ():
     try:
-        while True:
-            print('Hello')
+        op = ''
+        while(op != '2'):
+            print('Escolha uma opção: ')
+            print('1 - iniciar o jogo')
+            print('2 - sair')
+            op = input('> ')
+        print('Bye')
     except KeyboardInterrupt as e:
-        print('User interrupt, bye!')
-        
+        print('User interrupt. Bye!')
+
 if __name__ == '__main__':
     menu()
