47a415ae16c04904e3a7afb48027193e722e8fca
Wesley Xavier <wesley.xvier@gmail.com>
2025-03-31 20:56:48 -0300

    :bricks: chore: atualizando gameplay principal.

--- source/main.py
+++ source/main.py
@@ -1,9 +1,16 @@
+import create_table, util
 import menu, about, write_log as log
+import modules.wordpicker
 def main ():
     try:
         while(True):
             op = menu.main_menu()
             if op in ['q', 'Q'] : break
+            elif op == '1':
+                op = menu.game_menu()
+                if op == '1':
+                    modules.wordpicker.game()
+                elif op == 'q': break
             elif op == '3': about.about()
     except KeyboardInterrupt:
         print('User interrupt. Bye!')
@@ -11,3 +18,4 @@ def main ():
 
 if __name__ == '__main__':
     main()
+    create_table.main()
